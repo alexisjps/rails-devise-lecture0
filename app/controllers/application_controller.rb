9bbaf1e6f57939c39ed26963879cbaf7bd9e072a
alexis stephan <alexis-stephan@hotmail.fr>
2024-03-11 10:39:07 +0100

    rails new + setup devise with restaurants
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+ # ici je code un coportement spéficique sign_in sign_up
+ before_action :authenticate_user!
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+ private
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age])
+ end
 end
